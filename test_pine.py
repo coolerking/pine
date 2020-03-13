30ad6769a18a268509b398ae2091be6f2f596a00
Tasuku Hori <tasuku-hori@exa-corp.co.jp>
2020-03-13 09:32:57 +0900

    RS素データテスト

--- test_pine.py
+++ test_pine.py
@@ -64,9 +64,29 @@ def test_pine_realsense2():
     except KeyboardInterrupt:
         print('stopped')
 
+def test_pine_rs():
+    cfg = dk.load_config()
+    V = dk.vehicle.Vehicle()
+    
+    from pine.realsense2 import PoseReader2
+    V.add(PoseReader(cfg), outputs=['pose/x', 'pose/y', 'pose/z', 'pose/ang_x', 'pose/ang_y', 'pose/ang_z'])
+
+    class Prt:
+        def run(self, x, y, z, ax, ay, az):
+            print('{:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}, {:.3f}'.format(x, y, z, ax, ay, az))
+
+    V.add(Prt(), inputs=['pose/x', 'pose/y', 'pose/z', 'pose/ang_x', 'pose/ang_y', 'pose/ang_z'])
+
+    try:
+        V.start(rate_hz=20, max_loop_count=3600)
+    except KeyboardInterrupt:
+        print('stopped')
+
 if __name__ == '__main__':
-    print('[start] double hedges')
-    test_pine_double_hedges()
-    sleep(5.0)
-    print('[start] realsense2')
-    test_pine_realsense2()
+    #print('[start] double hedges')
+    #test_pine_double_hedges()
+    #sleep(5.0)
+    #print('[start] realsense2')
+    #test_pine_realsense2()
+
+    test_pine_rs()
