9cadc8d6f279063412a7856f23a3bb3a62f4f884
Tasuku Hori <tasuku-hori@exa-corp.co.jp>
2020-03-19 10:01:21 +0900

    hedges化

--- test_pine.py
+++ test_pine.py
@@ -174,10 +174,16 @@ def test_angle():
     cfg = dk.load_config()
     V = dk.vehicle.Vehicle()
 
+    '''
     from pine.realsense2 import PoseReader
     V.add(PoseReader(cfg), outputs=['pose/real/x', 'pose/real/y', 'pose/real/angle'])
 
     V.add(PrintPose(), inputs=['pose/real/x', 'pose/real/y', 'pose/real/angle'])
+    '''
+    from pine.double_hedges import PoseReader
+    V.add(PoseReader(cfg), outputs=['pose/hedges/x', 'pose/hedges/y', 'pose/hedges/angle'])
+
+    V.add(PrintPose(), inputs=[['pose/hedges/x', 'pose/hedges/y', 'pose/hedges/angle'])
 
     try:
         V.start(rate_hz=cfg.DRIVE_LOOP_HZ, 
